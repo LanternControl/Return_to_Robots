533155c5ae115599b27cede572abe916fd25a6f0
LanternControl <brett4president@hotmail.com>
2017-08-28 14:12:52 -0400

    Added index some CSS and going to integrate db next

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const mustache = require('mustache-express');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const mongoURL = 'mongodb://localhost:27017/newdb';
+
+//need to change this to work with db
+// const data = require('./data.js');
+
+const app = express();
+
+
+app.engine('mustache', mustache());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(express.static(__dirname + '/public'));
+
+
+app.use('/', function (req, res) {
+ MongoClient.connect(mongoURL, function (err, db) {
+  const robots = db.collection('robots');
+  robots.find({}).toArray(function (err, docs) {
+   res.render("index", {robots: docs})
+  })
+ })
+})
+
+
+
+
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+});
